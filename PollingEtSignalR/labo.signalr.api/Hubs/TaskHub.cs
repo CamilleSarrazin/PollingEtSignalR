0f553521dac12c315fc306e1aad81fe8bfe9fa30
Camille <1373080@cegepmontpetit.ca>
2024-09-13 17:49:04 -0400

    FCT Exercice Polling et SignalR
    Exercice complété

---
+++ PollingEtSignalR/labo.signalr.api/Hubs/TaskHub.cs
@@ -0,0 +1,55 @@
+﻿using labo.signalr.api.Data;
+using Microsoft.AspNetCore.SignalR;
+
+namespace labo.signalr.api.Hubs
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+
+    public class TaskHub:Hub
+    {
+        ApplicationDbContext _context;
+
+        public TaskHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //Déclencher la fonction TaskList sur le client qui a fait l’appel
+            await base.OnConnectedAsync();
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await Clients.Caller.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task AddTask(string task)
+        {
+            //Ajouter une nouvelle tâche dans la BD, puis déclencher la fonction TaskList sur tous les clients
+            _context.UselessTasks.Add(new Models.UselessTask() { Text = task });
+            _context.SaveChanges();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public async Task CompleteTask(int taskid)
+        {
+            //Marquer une tâche comme complétée dans BD, puis déclencher la fonction TaskList sur tous les clients
+            var task = _context.UselessTasks.Single(t => t.Id == taskid);
+            task.Completed = true;
+            _context.SaveChanges();
+            await Clients.All.SendAsync("TaskList", _context.UselessTasks.ToList());
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            //Décrémenter le nombre d'utilisateurs actifs
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            //Déclencher la fonction UserCount sur les clients
+            await Clients.All.SendAsync("UserCount", UserHandler.ConnectedIds.Count);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UselessTask } from '../models/UselessTask';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-polling',
@@ -9,6 +10,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class PollingComponent implements OnInit {
 
+  apiUrl = "https://localhost:7289/api/";
   title = 'labo.signalr.ng';
   tasks: UselessTask[] = [];
   taskname: string = "";
@@ -19,16 +21,20 @@ export class PollingComponent implements OnInit {
     this.updateTasks();
   }
 
-  complete(id: number) {
+  async complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+    return await lastValueFrom(this.http.get<any>(this.apiUrl+'UselessTasks/Complete/' + id));
   }
 
-  addtask() {
+  async addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    this.tasks.push(await lastValueFrom(this.http.post<UselessTask>(this.apiUrl+'UselessTasks/Add?taskText=' + this.taskname, null)));
   }
 
-  updateTasks() {
+  async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
+    this.tasks = await lastValueFrom(this.http.get<UselessTask[]>(this.apiUrl+'UselessTasks/GetAll'));
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    setTimeout(() => this.updateTasks(), 1000);
   }
 }
